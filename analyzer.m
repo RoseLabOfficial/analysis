424fdcebb6d4a44ba4723ab4ef325d705c690772
Sith'ari <ralluri.utah@gmail.com>
2022-09-08 17:08:02 -0600

    code vectorization and added missing code for adjusting Vm as per Ess

--- analyzer.m
+++ analyzer.m
@@ -1,7 +1,7 @@
 filename = "example_data2.xlsx"; % The path+name of the file goes here.
 paradigms = ["10pps", "20pps", "40pps"]; % Different rates in the file go here; these should be the same as the worksheets in the file.
 active_duration = [0.6, 0.4, 0.35]; % How long does the response to simulus last at each pulse rate? (secs).
-rec = WholeCellRecording(filename, paradigms, active_duration);
+rec = WholeCellRecording2(filename, paradigms, active_duration);
 filter_parameters.CutOffFrequency = 150; % The lower this value, the smoother the traces get, but stay above 80 (Hz) for now.
 filter_parameters.FilterOrder = 100;
 filter_parameters.PassbandRipple = 0.01;
@@ -20,3 +20,5 @@ rec = rec.compute_passive_conductances();
 % fprintf("Stats for: %s \n", filename); 
 % disp(stats);
 % rec = rec.dynamics_plots();
+figure('Name', 'WholeCellRecording2');
+plot(rec(1, 1).time, rec(1, 1).ge, 'r', rec(1, 1).time, rec(1, 1).gi, 'b');
--- analyzer_example.m
+++ analyzer_example.m
@@ -1,21 +1,23 @@
-filename = "example_data.xlsx"; % The path+name of the file goes here.
-rates = ["5pps", "10pps", "20pps", "40pps", "60pps"]; % Different rates in the file go here; these should be the same as the worksheets in the file.
-active_duration = [1.2, 0.6, 0.4, 0.4, 0.4]; % How long does the response to simulus last at each pulse rate? (secs).
+filename = "example_data2.xlsx"; % The path+name of the file goes here.
+rates = ["10pps", "20pps", "40pps"]; % Different rates in the file go here; these should be the same as the worksheets in the file.
+active_duration = [0.6, 0.4, 0.4]; % How long does the response to simulus last at each pulse rate? (secs).
 filter_parameters.CutOffFrequency = 150; % The lower this value, the smoother the traces get, but stay above 80 (Hz) for now.
 filter_parameters.FilterOrder = 100;
 filter_parameters.PassbandRipple = 0.01;
 filter_parameters.StopbandAttenuation = 80;
-rec = WholeCellRecording(filename, rates, active_duration);
-rec = rec.zero_phase_filter_Vm(filter_parameters);
-rec = rec.compute_active_conductances();
-rec = rec.compute_active_currents();
-rec = rec.compute_leakage_currents();
-rec = rec.compute_membrane_currents();
+data = WholeCellRecording(filename, rates, active_duration);
+data = data.zero_phase_filter_Vm(filter_parameters);
+data = data.compute_active_conductances();
+data = data.compute_active_currents();
+data = data.compute_leakage_currents();
+data = data.compute_membrane_currents();
 filter_parameters.CutOffFrequency = 20;
-rec = rec.zero_phase_filter_Im(filter_parameters);
-rec = rec.compute_passive_conductances();
-rec = rec.plots();
-[rec, stats] = rec.generate_stats();
-fprintf("Stats for: %s \n", filename); 
-disp(stats);
+data = data.zero_phase_filter_Im(filter_parameters);
+data = data.compute_passive_conductances();
+% rec = rec.plots();
+% [rec, stats] = rec.generate_stats();
+% fprintf("Stats for: %s \n", filename); 
+% disp(stats);
 % rec = rec.dynamics_plots();
+figure('Name', 'WholeCellRecording');
+plot(data(1).time, data(1).ge, 'r', data(1).time, data(1).gi, 'b');
