a25fc0c9288e14e431dddfb8008011a004888ed1
Arikill <ralluri.utah@gmail.com>
2024-03-02 15:35:05 -0700

    low pass filtering, not working yet.

--- libs/systems.py
+++ libs/systems.py
@@ -11,6 +11,13 @@ class WholeCellRecording:
             self.data[inj] = ((self.data[inj] - self.data[inj][0])*1e-3)+self.parameters["Ess"][idx]
         return self.data
     
+    def filter_data(self, cutoff: float = 100.0, stopband_attenuation: float = 80, passband_ripple: float = 0.01):
+        sampling_rate = 1/(self.data["times"][1] - self.data["times"][0])
+        filter = LowPassFilter(sampling_rate)
+        for idx, inj in enumerate(self.parameters["Iinj"]):
+            self.data["filtered_"+str(inj)] = filter.filter(self.data[inj], cutoff, stopband_attenuation, passband_ripple)
+        return self.data
+    
     def compute_active_conductance_constants(self):
         self.parameters["alpha"] = (1.0/self.parameters["Rin"])/(self.parameters["Et"] - self.parameters["Eact"])
         self.parameters["beta"] = self.parameters["alpha"]*(self.parameters["Et"] - self.parameters["Er"])
@@ -39,7 +46,22 @@ class WholeCellRecording:
     def compute_membrane_currents(self):
         for idx, clamp in enumerate(self.parameters["Iinj"]):
             self.data["Im_"+str(clamp)] = self.parameters["Cm"][idx]*self.data[clamp].diff()
-            self.data["Im_"+str(clamp)][0] = 0.0
+            self.data.at[0, "Im_"+str(clamp)] = 0.0
         return self.data
     
+class LowPassFilter:
+    def __init__(self, sampling_rate: float):
+        self.sampling_rate = sampling_rate
+        pass
+
+    def compute_minimum_order(self, cutoff: float, stopband_attenuation: float, passband_ripple: float):
+        passband_edge_frequency = cutoff/(self.sampling_rate/2)
+        stopband_edge_frequency = 1.5*passband_edge_frequency
+        order, normalized_cutoff_frequency = filters.buttord(passband_edge_frequency, stopband_edge_frequency, 
+                                                            passband_ripple, stopband_attenuation)
+        return order, normalized_cutoff_frequency
     
+    def filter(self, input: float, cutoff: float, stopband_attenuation: float, passband_ripple: float):
+        order, normalized_frequency = self.compute_minimum_order(cutoff, stopband_attenuation, passband_ripple)
+        second_order_sections = filters.butter(order, normalized_frequency, output="sos")
+        return filters.sosfilt(second_order_sections, input)
--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 from libs.readers import XLReader
 from libs.systems import WholeCellRecording
 import numpy as np
+import matplotlib.pyplot as plt
 if __name__ == "__main__":
     filename = "./test_data/2012-05-03_2_baseline_prr_v4.xlsx"
     reader = XLReader(filename)
     data = WholeCellRecording(reader.get_paradigm_data("10pps"), 
                               reader.get_paradigm_parameters("10pps"))
     data.scale_data()
+    data.filter_data()
     data.compute_polarizations()
     data.compute_active_conductance_constants()
     data.compute_active_currents()
     data.compute_leakage_currents()
-    print(data.compute_membrane_currents())
+    data.compute_membrane_currents()
+    data.data[[0, "filtered_0.0"]].plot()
+    plt.show()
