bedc971ea6dc55deb30bde65db5d315fca565ea8
Sith'ari <ralluri.utah@gmail.com>
2024-03-08 22:52:38 -0700

    adding input directory to filenames in batch processing

--- test.py
+++ test.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     parser.add_argument("--list", "-l", type=str, default=None, help="list of all the files in input directory")
     parser.add_argument("--test", "-t", type=str, default=None, help="perform a test run to verify analysis.")
     args = parser.parse_args()
-    print(args)
     if args.configs is not None:
         configs = Configurations(args.configs)
         files = configs.get_files_to_analyze()
         for file_id in files:
-            reader = XLReader(file_id)
-            for paradigm in reader.get_paradigms():
-                data = WholeCellRecording(reader.get_paradigm_data(paradigm), 
-                                reader.get_paradigm_parameters(paradigm))
-                data.estimate_conductances(membrane_voltage_filter_cutoff=200, active_currents_filter_cutoff=100, membrane_currents_filter_cuoff=60)
+            print(configs.get_input_directory()+"/"+file_id)
+            # reader = XLReader(file_id)
+            # for paradigm in reader.get_paradigms():
+            #     data = WholeCellRecording(reader.get_paradigm_data(paradigm), 
+            #                     reader.get_paradigm_parameters(paradigm))
+            #     data.estimate_conductances(membrane_voltage_filter_cutoff=200, active_currents_filter_cutoff=100, membrane_currents_filter_cuoff=60)
