0a16827923f6b41a8abbc12607d1ad80c29e9501
Arikill <ralluri.utah@gmail.com>
2024-03-15 18:28:35 -0600

    building named argument inputs in terminal.

--- libs/sysops.py
+++ libs/sysops.py
@@ -1,15 +1,21 @@
 import json
 import os
+from pathlib import Path
 
 class SystemOperations:
     def make_directory(self, directory):
         if not os.path.exists(directory):
             os.makedirs(directory)
     
-    def read_config_json(self, jsonfile: str):
+    def read_json(self, jsonfile: str):
         with open(jsonfile, "r") as settings:
             configs = json.load(settings)
         return configs
+
+    def write_json(self, jsonfile: str, json_data: dict):
+        with open(jsonfile, "w") as settings:
+            configs = json.dump(json_data, indent=4)
+        pass
     
     def check_directory(self, directory):
         return os.path.exists(directory)
@@ -19,19 +25,32 @@ class SystemOperations:
 
 class Configurations:
     def __init__(self, configs_json: str):
+        self.configs_json = configs_json
         self.sysops = SystemOperations()
-        self.configs = self.sysops.read_config_json(configs_json)
+        self.settings = self.sysops.read_json(configs_json)
+        pass
+
+    def set_input_directory(self, inputdir: Path):
+        self.settings["inputdir"] = inputdir
+        self.sysops.write_json(self.configs_json, self.settings)
         pass
 
     def get_input_directory(self):
-        return self.configs["inputdir"]
+        return self.settings["inputdir"]
+
+    def set_output_directory(self, outputdir: Path):
+        self.settings["outputdir"] = outputdir
+        self.sysops.write_json(self.configs_json, self.settings)
+        pass
     
     def get_output_director(self):
-        return self.configs["outputdir"]
+        return self.settings["outputdir"]
+
+    def get_input_files(self):
+        return self.sysops.list_files(self.get_input_directory())
     
     def get_files_to_skip(self):
-        return self.configs["skiplist"]
+        return self.settings["skiplist"]
     
     def get_files_to_analyze(self):
-        files = self.sysops.list_files(self.get_input_directory())
-        return [filename for filename in files if filename not in self.get_files_to_skip()]
+        return [filename for filename in self.get_input_files() if filename not in self.get_files_to_skip()]
--- test.py
+++ test.py
@@ -4,19 +4,62 @@ from libs.sysops import *
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import logging
+import os
+from pathlib import Path
 
+def init_logging():
+    current_file = os.path.splitext(os.path.basename(__file__))[0]
+    logging.basicConfig(filename=current_file+".log", level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+
+def load_configurations():
+    configurations_json: Path = "./settings/configurations.json"
+    return Configurations(configurations_json)
+    
+
+def main():
+    configs = load_configurations()
+    parser = argparse.ArgumentParser(description="Passing named arguments to analyzer")
+    parser.add_argument("--show_configs", "-sc", action="store_true", help="show configurations")
+    parser.add_argument("--list_inputs", "-li", action="store_true", help="list files from the input directory")
+    parser.add_argument("--list_skips", "-ls", action="store_true", help="list files that are specified to be skipped in configurations")
+    parser.add_argument("--list_files", "-la", action="store_true", help="list files that will be analyzed as per current configuraitons")
+    parser.add_argument("--set_inputdir", "-si", type=Path, default ="./inputs", help="set input directory in configurations")
+    parser.add_argument("--set_outputdir", "-so", type=Path, default ="./outputs", help="set output directory in configurations")
+    args = parser.parse_args()
+    if args.show_configs:
+        print(configs.settings)
+    if args.list_inputs:
+        print(configs.get_input_files())
+    if args.list_skips:
+        print(configs.get_files_to_skip())
+    if args.list_files:
+        print(configs.get_files_to_analyze())
+    if args.set_inputdir is not None:
+        configs.set_input_directory(args.set_inputdir)
+        print(configs.settings)
+    if args.set_outputdir is not None:
+        configs.set_output_directory(args.set_outputdir)
+        print(configs.settings)
+    
+        
+    
+    
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Passing named arguments to analyzer.")
-    parser.add_argument("--configs", "-c", type=str, default="./settings/batch_processing.json", help="json file specifying inputdir and outputdir")
-    parser.add_argument("--list", "-l", type=str, default=None, help="list of all the files in input directory")
-    parser.add_argument("--test", "-t", type=str, default=None, help="perform a test run to verify analysis.")
-    args = parser.parse_args()
-    if args.configs is not None:
-        configs = Configurations(args.configs)
-        files = configs.get_files_to_analyze()
-        for file_id in files:
-            print(configs.get_input_directory()+"/"+file_id)
+    main()
+    # parser = argparse.ArgumentParser(description="Passing named arguments to analyzer.")
+    # parser.add_argument("--show_configs", "-sc", action="store_file", help="show configurations")
+    # parser.add_argument("--show_inputs", "-si", action="store_file
+    # parser.add_argument("--configs", "-c", type=str, default="./settings/batch_processing.json", help="json file specifying inputdir and outputdir")
+    # parser.add_argument("--list", "-l", type=str, default=None, help="list of all the files in input directory")
+    # parser.add_argument("--test", "-t", type=str, default=None, help="perform a test run to verify analysis.")
+    # args = parser.parse_args()
+    # if args.configs is not None:
+    #     configs = Configurations(args.configs)
+    #     files = configs.get_files_to_analyze()
+    #     for file_id in files:
+    #         print(configs.get_input_directory()+"/"+file_id)
             # reader = XLReader(file_id)
             # for paradigm in reader.get_paradigms():
             #     data = WholeCellRecording(reader.get_paradigm_data(paradigm), 
