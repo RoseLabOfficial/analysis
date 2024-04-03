66aaf138388dec9bef1a0e6d17894abf1f9eee47
Arikill <ralluri.utah@gmail.com>
2024-04-02 18:02:49 -0600

    refactoring

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,4 +1,28 @@
-import numpy as np
 import pandas as pd
-import scipy.signal as filters
-import matplotlib.pyplot as plt
+import numpy as np
+import os
+import openpyxl
+import logging.config
+import json
+from pathlib import Path
+
+def init_logging(logger_configurations):
+    logging.config.dictConfig(logger_configurations)
+
+class JsonHandler:
+    def __init__(self):
+        pass
+
+    def read(self, fileaddress: Path):
+        try:
+            with open(fileaddress, 'r') as filepointer:
+                return json.load(filepointer)
+        except FileNotFoundError:
+            raise FileNotFoundError(f'File not found: {fileaddress}')
+        except json.JSONDecodeError as e:
+            raise ValueError(f'Failed to decode JSON from {fileaddress}: {e.msg}')
+
+jh = JsonHandler()
+logger_configurations = jh.read("./settings/logging.json")
+init_logging(logger_configurations)
+analysis_logger = logging.getLogger('Analyzer')
--- libs/utils.py
+++ libs/utils.py
@@ -1,29 +1,22 @@
-import pandas as pd
-from pathlib import Path
-import os
-import json
-import openpyxl
+from . import *
 
-class Compliance:
+class SystemOperations:
     def __init__(self):
         pass
 
-    def check_parameter_compliance(self, parameters):
-        return all(key in parameters for key in ("Iinj", "Cm", "Rin", "Er", "Ee", "Ei", "Et", "Ess", "xalpha", "xbeta", "sps", "Eref", "rate", "npulses", "amplitude"))
-
-    def check_compliance(self, data: pd.DataFrame, parameters: pd.DataFrame):
-        if self.check_parameter_compliance():
-            for clamp in parameters["Iinj"]:
-                if not clamp in data:
-                    return False
-        return True
+    def does_exist(self, dir_or_file_with_path: Path):
+        return os.path.exists(dir_or_file_with_path)
     
+    def list_files(self, dir: Path):
+        if self.does_exist(dir):
+            return os.listdir(dir)
+        else:
+            raise FileExistsError(f"{dir} does not exist!")
+
 class FileTypeChecker:
     def __init__(self):
         self.system_operations = SystemOperations()
         pass
-
-    
 
     def is_xlsx(self, file_with_path: Path):
         if self.system_operations.does_exist(file_with_path) and file_with_path.endswith('.xlsx'):
@@ -44,15 +37,16 @@ class FileTypeChecker:
                 return False
         return False
 
-class SystemOperations:
+class Compliance:
     def __init__(self):
         pass
 
-    def does_exist(self, dir_or_file_with_path: Path):
-        return os.path.exists(dir_or_file_with_path)
-    
-    def list_files(self, dir: Path):
-        if self.does_exist(dir):
-            return os.listdir(dir)
-        else:
-            raise FileExistsError(f"{dir} does not exist!")
+    def check_parameter_compliance(self, parameters):
+        return all(key in parameters for key in ("Iinj", "Cm", "Rin", "Er", "Ee", "Ei", "Et", "Ess", "xalpha", "xbeta", "sps", "Eref", "rate", "npulses", "amplitude"))
+
+    def check_compliance(self, data: pd.DataFrame, parameters: pd.DataFrame):
+        if self.check_parameter_compliance():
+            for clamp in parameters["Iinj"]:
+                if not clamp in data:
+                    return False
+        return True
